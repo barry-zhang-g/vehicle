ac068cb5ba0c751432b551af5e607223fc93f2cc
barry <barry-zhang@outlook.com>
2021-02-10 21:06:14 +0800

    restore2

--- vehicle.location/src/main/java/vehicle/location/Location.java
+++ vehicle.location/src/main/java/vehicle/location/Location.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class Location {
 	public static void main(String[] args) throws Exception {
-		//first eureka server
 		SpringApplication.run(Location.class, args);
 	}
 }
