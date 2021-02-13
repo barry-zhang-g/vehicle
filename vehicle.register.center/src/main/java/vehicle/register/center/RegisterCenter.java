e116a7a9db030c67e3eee7cdc41e6f577ca3e53a
barry <barry-zhang@outlook.com>
2021-02-13 13:45:29 +0800

    repeat init

---
+++ vehicle.register.center/src/main/java/vehicle/register/center/RegisterCenter.java
@@ -0,0 +1,13 @@
+package vehicle.register.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterCenter {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RegisterCenter.class, args);
+	}
+}
