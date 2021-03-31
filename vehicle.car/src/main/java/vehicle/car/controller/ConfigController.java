5a0d66698e315d7cd39f32fec6899ccf43cfe520
barry <barry-zhang@outlook.com>
2021-03-31 21:12:33 +0800

    configcontroller

--- vehicle.car/src/main/java/vehicle/car/controller/ConfigController.java
+++ vehicle.car/src/main/java/vehicle/car/controller/ConfigController.java
@@ -1,14 +1,10 @@
 package vehicle.car.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import vehicle.car.config.ConfigInfo;
-import vehicle.car.service.CarService;
 
 @RestController
 public class ConfigController {
